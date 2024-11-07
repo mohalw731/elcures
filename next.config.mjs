400cb0a78b46ca2c312463d55feb71fb668747f5
mohalw731 <mohalw731@gmail.com>
2024-11-07 17:55:08 +0100

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Enable static export by setting up the trailingSlash option (optional)
+    exportTrailingSlash: true,
+    
+    // Optionally disable image optimization if you don't want Next.js to optimize images during static export
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
